913a5820d24ce7c6db207bedef3ffea684f18576
Jerome Leclanche <jerome@leclan.ch>
2015-12-25 14:50:45 +0200

    Add a script to generate all builds

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ HSDATA_URL="https://github.com/HearthSim/hs-data.git"
 echo "Fetching data files from $HSDATA_URL into $HSDATA_DIR"
 
 if [ ! -e "$HSDATA_DIR" ]; then
-	git clone --depth=1 "$HSDATA_URL" "$HSDATA_DIR"
+	git clone "$HSDATA_URL" "$HSDATA_DIR"
 else
 	git -C "$HSDATA_DIR" fetch &&
 	git -C "$HSDATA_DIR" reset --hard origin/master
---
+++ make_all_builds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASEDIR="$(readlink -f $(dirname $0))"
+HSDATADIR="$BASEDIR/hs-data"
+
+for tag in $(git -C "$HSDATADIR" tag); do
+	git -C "$HSDATADIR" reset --hard "$tag"
+	python "$BASEDIR/generate.py" --input-dir="$HSDATADIR" --output-dir="$BASEDIR/out/$tag"
+done
