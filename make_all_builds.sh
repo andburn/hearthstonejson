b99b8079fc47dfa3617628adfb0cf84dc213e594
Jerome Leclanche <jerome@leclan.ch>
2016-02-17 02:50:17 +0200

    Auto-symlink latest/ to the highest build

--- make_all_builds.sh
+++ make_all_builds.sh
@@ -8,3 +8,9 @@ for tag in $(git -C "$HSDATADIR" tag); do
 	git -C "$HSDATADIR" reset --hard "$tag"
 	python "$BASEDIR/generate.py" --input-dir="$HSDATADIR" --output-dir="$BASEDIR/out/$tag"
 done
+
+# Symlink latest build
+builds=($(basename -a "$BASEDIR/out/"*))
+IFS=$'\n'
+maxbuild=$(echo "${builds[*]}" | sort -nr | head -n1)
+ln -s "$maxbuild" "$BASEDIR/out/latest"
