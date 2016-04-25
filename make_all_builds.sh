61c80160c1760184a8d41d6ebae070319be31aaa
Jerome Leclanche <jerome@leclan.ch>
2016-04-25 07:08:40 +0300

    Update build script to sync serverside data

--- make_all_builds.sh
+++ make_all_builds.sh
@@ -3,7 +3,8 @@ set -e
 
 BASEDIR="$(readlink -f $(dirname $0))"
 HSDATADIR="$BASEDIR/hs-data"
-OUTDIR="$BASEDIR/out"
+OUTDIR="$BASEDIR/v1"
+LIVEDIR="/srv/http/api.hearthstonejson.com/html/v1"
 
 builds=($(printf "%s\n" $(git -C "$HSDATADIR" tag) | sort -n))
 maxbuild="${builds[-1]}"
@@ -15,5 +16,8 @@ for tag in ${builds[@]}; do
 	python "$BASEDIR/generate.py" --input-dir="$HSDATADIR" --output-dir="$OUTDIR/$tag"
 done
 
+sudo rsync -rtv "$OUTDIR" "$LIVEDIR"
+sudo rm "$LIVEDIR/latest"
 # Symlink latest build
-ln -s "$maxbuild" "$OUTDIR/latest"
+sudo ln -s "$maxbuild" "$LIVEDIR/latest"
+sudo chown -R www-data:www-data "$LIVEDIR"
